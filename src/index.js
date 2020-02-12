f5e6296c039914413e801e236c94e176cffdcd79
ellie25v <yelizarieva.ellie@gmail.com>
2020-02-12 09:03:23 +0200

    a

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import './styles.css';
+
+const colors = [
+    '#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+		  '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+		  '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+		  '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+		  '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+		  '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+		  '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
+		  '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+		  '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
+		  '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'
+];
+
+const refs = {
+    body: document.querySelector('body'),
+    btnStart: document.querySelector('button[data-action="start"]'),
+    btnStop: document.querySelector('button[data-action="stop"]')
+};
+
+console.log(refs);
+
+const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+let timerId = null;
+
+refs.btnStart.addEventListener("click", onStart);
+
+function onStart(e) {
+    timerId = setInterval(() => {
+        let color = randomIntegerFromInterval(0, colors.length - 1);
+        refs.body.bgColor = colors[color];
+        console.log(5)
+    }, 200);
+    console.log(timerId)
+    refs.btnStart.removeEventListener("click", onStart);
+}
+
+refs.btnStop.addEventListener("click", () => {
+    clearInterval(timerId);
+    console.log("setInterval stopped!");
+    refs.btnStart.addEventListener("click", onStart);
+});
